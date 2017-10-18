e2ab994e081f5ab31227d6bb108d0a3da76263d1
Elkantor <vic03@live.fr>
2017-10-18 15:03:58 +0200

    add player and camera

---
+++ Assets/PlayerModule/Scripts/Loader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+    public Texture2D noise = null;
+
+    // Use this for initialization
+    void Start () {
+        //Texture2D tex;
+        //tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
+        //string path = Application.dataPath;
+        //tex = LoadPNG("./PlayerController/Textures/T_Noise.png");
+        noise.LoadImage(noise.GetRawTextureData());
+        Color colorPixel = noise.GetPixel(0, 0);
+        //Debug.Log("color pixel : " + colorPixel.ToString());
+        Debug.Log("color pixel : " + noise.format);
+    }
+
+    public static Texture2D LoadPNG(string filePath)
+    {
+
+        Texture2D tex = null;
+        byte[] fileData;
+
+        if (File.Exists(filePath))
+        {
+            fileData = File.ReadAllBytes(filePath);
+            tex = new Texture2D(2, 2);
+            tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+        }
+        return tex;
+    }
+}
