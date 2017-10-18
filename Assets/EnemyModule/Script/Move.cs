a3441b8b1934be60e12a027e0134134a90437ade
Antoine <antoine@salutmdr.local>
2017-10-18 10:38:57 +0200

    MaJ script

---
+++ Assets/EnemyModule/Script/Move.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () 
+	{
+		MoveC(GetPlayerMove ());
+	}
+
+	/// <summary>
+	/// Effectue le déplacement du personnage en fonction des inputs du joueur.
+	/// </summary>
+	public void MoveC(Vector3 direction)
+	{
+		//Si la direction entrée est inverse à la direction courante
+		/*if (direction.x * transform.right.x < 0)
+		{
+			//on retourne le personnage.
+			transform.Rotate(0, 180, 0);
+		}*/
+
+		this.GetComponent<Rigidbody>().velocity = direction*10;
+
+
+	}
+
+	/// <summary>
+	/// recuperer la direction des joueurs lorsqu'on appuie sur les touches vertical et horizontal
+	/// </summary>
+	/// <returns>Vecteur avec la direction</returns>
+	public Vector3 GetPlayerMove()
+	{
+		float vertical = Input.GetAxisRaw("Vertical");
+		float horizontal = Input.GetAxisRaw("Horizontal");
+
+		return new Vector3(horizontal, 0, vertical);
+	}
+}
