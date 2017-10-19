fee06c55ba75de1738894fc74e7dcbf1f28a8a35
fannil <fannil.iut@gmail.com>
2017-10-19 18:42:24 +0200

    Ajout du module menu dans les assets

---
+++ Assets/MenuModule/Script/OnButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnButton : MonoBehaviour
+{
+    public GameObject BOff;
+
+    public void OnClikButton()
+    {
+        Debug.Log("toto");
+        Button bOff = BOff.GetComponent<Button>();
+        ColorBlock cbOff = bOff.colors;
+        cbOff.normalColor = new Color((float)128 / 255, (float)128 / 255, (float)128 / 255, (float)150 / 255);
+        bOff.colors = cbOff;
+        Button bOn = GetComponent<Button>();
+        ColorBlock cbOn = bOn.colors;
+        cbOn.normalColor = new Color((float)100 / 255, (float)146 / 255, (float)210 / 255);
+        cbOn.highlightedColor = new Color((float)100 / 255, (float)146 / 255, (float)210 / 255);
+        bOn.colors = cbOn;
+
+    }
+}
