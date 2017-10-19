29b10672a49a325679e4293876292f9fb5be0eca
Antoine <antoine@salutmdr.local>
2017-10-19 17:29:26 +0200

    Generation des pitit monstre TROP choupi

--- Assets/EnemyModule/Script/SpawnEntities.cs
+++ Assets/EnemyModule/Script/SpawnEntities.cs
@@ -24,7 +24,7 @@ public class SpawnEntities : MonoBehaviour {
 				minT = new Vector2 (t.Value.Terrain.transform.position.x - boundsSize.x/2, t.Value.Terrain.transform.position.z - boundsSize.z/2);
 				maxT = new Vector2 (t.Value.Terrain.transform.position.x + boundsSize.x/2, t.Value.Terrain.transform.position.z + boundsSize.z/2);
 				foreach (GameObject e in entities) {
-					for (int i = 0; i < 10; i++) {
+					for (int i = 0; i < 1; i++) {
 						GameObject g = Instantiate (e);
 						g.transform.position = new Vector3 (Random.Range (minT.x, maxT.x), player.transform.position.y, Random.Range (minT.y, maxT.y));
 						g.transform.SetParent (t.Value.Terrain.transform);
