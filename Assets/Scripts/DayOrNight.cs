c8f76fe0bf5ceef702c9011aa19834ca1fc9c4e0
ATTARD Yannis <Yannis.attard@etudiant.cnam-enjmin.fr>
2017-10-19 15:40:17 +0200

    Added terrain prefabs, changed terrain materials, added day or night scripts

---
+++ Assets/Scripts/DayOrNight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayOrNight : MonoBehaviour {
+
+    public Material dayBox;
+    public Material nightBox;
+
+	// Use this for initialization
+	void Start () {
+        if(Random.Range(0, 2) == 0)
+        {
+            RenderSettings.skybox = dayBox;
+        }
+        else
+        {
+            RenderSettings.skybox = nightBox;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
