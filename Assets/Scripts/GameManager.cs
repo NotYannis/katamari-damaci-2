50d922e202164d9f3d971c2448df435cedbfdae8
Clement-R <rondeau.clement@gmail.com>
2017-10-19 14:14:06 +0200

    Add pause menu key detection

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public GameObject pauseMenu;
+
     static public float squashRate = 0.05f;
     private float _lastSquashRate = 0.05f;
-
+    
     void Update() {
         
+        if(Input.GetKeyDown(KeyCode.P)) {
+            if(pauseMenu.activeSelf) {
+                pauseMenu.SetActive(false);
+            } else {
+                pauseMenu.SetActive(true);
+            }
+        }
+
         if (squashRate != _lastSquashRate) {
             EventManager.TriggerEvent("squashValueChanged");
         }
