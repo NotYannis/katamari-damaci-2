a6ae8f3312893cb457b19841b85cfd334b8c5deb
Clement-R <rondeau.clement@gmail.com>
2017-10-20 09:57:13 +0200

    Add header file for Wwise

---
+++ Assets/StreamingAssets/Wwise_IDs.h
@@ -0,0 +1,77 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBIANCE = 2981377429U;
+        static const AkUniqueID ECRASER = 2785223928U;
+        static const AkUniqueID GONFLER = 3398134696U;
+        static const AkUniqueID MENU = 2607556080U;
+        static const AkUniqueID MUSIC_SWITCH = 2724869341U;
+        static const AkUniqueID PLAY = 1256202815U;
+        static const AkUniqueID ROULE_EAU = 1000013658U;
+        static const AkUniqueID ROULE_FEUILLE = 321915247U;
+        static const AkUniqueID ROULE_HERBE = 379428675U;
+        static const AkUniqueID VOLUME_MUSIC_CONTROL = 3477848037U;
+        static const AkUniqueID VOLUME_SFX_CONTROL = 83106753U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace AMBIANCE
+        {
+            static const AkUniqueID GROUP = 2981377429U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID JOUR = 3896524701U;
+                static const AkUniqueID NUIT = 733794917U;
+            } // namespace SWITCH
+        } // namespace AMBIANCE
+
+        namespace MUSIC
+        {
+            static const AkUniqueID GROUP = 3991942870U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID JOUR = 3896524701U;
+                static const AkUniqueID NUIT = 733794917U;
+            } // namespace SWITCH
+        } // namespace MUSIC
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID VOLUME_CONTROL = 4194816893U;
+        static const AkUniqueID VOLUME_CONTROL_MUSIC = 2822349657U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID NEW_SOUNDBANK = 4072029455U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
